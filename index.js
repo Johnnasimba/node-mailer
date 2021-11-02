eaf7de0e3274e7e8d650d0336292a7b10087ca85
JOHN NASIMBA <nasimba4john@gmail.com>
2021-11-02 16:45:57 +0200

    Adding nodemailer and dotenv

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path =  require('path');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
+require('dotenv').config()
 
 const app = express();
 
@@ -39,7 +40,31 @@ app.post('/send', (req, res) => {
     <H3>Message</H3>
     <p> ${req.body.message}</p>       
     
-    `
+    `;
+    let transporter = nodemailer.createTransport({
+        host: "outlook.office365.com",
+        port: 995,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass, // generated ethereal password
+        },
+      });
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 })
 
 const PORT=process.env.PORT||3000;
